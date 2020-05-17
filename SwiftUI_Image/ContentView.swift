fe1f6d6ec9354bb91421d1a020d82714d1319e22
mohamed  habib <mohamedhabibjaberi111@gmail.com>
2020-05-17 23:11:25 +0200

    Add Modifiers to Image

--- SwiftUI_Image/ContentView.swift
+++ SwiftUI_Image/ContentView.swift
@@ -10,12 +10,32 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            //Spacer()
+            Image("Ray")
+                // .resizable()
+                .resizable(capInsets: EdgeInsets(), resizingMode: .tile)
+                .scaledToFill()
+                .saturation(0.7)
+                .contrast(100)
+                .hueRotation(Angle(degrees: 300))
+                .edgesIgnoringSafeArea([.top, .bottom])
+            //.aspectRatio(contentMode: .fit)
+            //   .scaledToFit()
+            //  .clipShape(Circle())
+            //     .mask(Image("Mascot").resizable())
+            // .padding([.leading, .trailing])
+   
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+            ContentView()
+                .previewDevice("iPhone SE")
+        }
     }
 }
